9a7d394b0f07387f4781f3d27eb639ff772aef07
Michelle <michellebrettcr@gmail.com>
2017-07-01 14:46:45 +1000

    Developed HTML Code

---
+++ Animal_adoption/scripts/script.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    startTab();
+};
+
+function startTab() {
+    document.getElementById("defaultOpen").click();
+}
+
+function openAnimal(evt, animal) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(animal).style.display = "block";
+    evt.currentTarget.className += " active";
+}

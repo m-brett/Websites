f2d4da5ef9a1aa9d9f9d84e8c033388dd8679d5a
Michelle <michellebrettcr@gmail.com>
2017-08-30 19:58:37 +1000

    Added Responsive Design for Mobile View

--- Photo_portfolio/scripts/scripts.js
+++ Photo_portfolio/scripts/scripts.js
@@ -1,17 +1,19 @@
 function openNav(num) {
     var sidebar = document.getElementById("side_bar"+num);
     var menu =  document.getElementById("menu"+num);
-    sidebar.style.width = "300px";
-    sidebar.style.height = "100vh";
-    menu.style.marginLeft = "300px";
-    menu.style.transition = "margin-left 0.5s";
+    if (window.innerWidth <= 784 ){
+      sidebar.style.width = "100%";
+      sidebar.style.height = "50vh";
+      menu.style.transition = "margin-left 0.5s";
+    } else if (window.innerWidth >= 785 ) {
+      sidebar.style.width = "300px";
+      sidebar.style.height = "100vh";
+      menu.style.marginLeft = "300px";
+      menu.style.transition = "margin-left 0.5s";
+    }
 }
 
 function closeNav(num) {
     document.getElementById("side_bar"+num).style.width = "0";
     document.getElementById("menu"+num).style.marginLeft= "0";
 }
-
-/*
-document.getElementById("side_bar"+num).addEventListener("wheel", closeNav);
-document.getElementById("main"+num).addEventListener("wheel", closeNav);*/
